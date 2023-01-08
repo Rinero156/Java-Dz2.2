a2b9d8dbf2e8a680adf8dd4bc283990f37b3855f
Konstantin S <97kostada@gmail.com>
2023-01-08 15:48:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 300;
+
+        int replenishment = 500;
+
+        int finalBalance;
+
+        if (replenishment > 100) {
+            int finalBonus = replenishment/100;
+
+            finalBalance = balance + replenishment + finalBonus;
+
+            System.out.println("Ваш баланс равен " + finalBalance);
+        } else {
+            finalBalance = balance + replenishment;
+            System.out.println("Ваш баланс равен " + finalBalance);
+        }
+
+    }
+}
